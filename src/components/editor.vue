8e92d8883d4a296f59284db8a61e65aabf865333
qingwei.li <cinwell.li@gmail.com>
2018-03-30 08:34:26 +0800

    update default demo

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -18,20 +18,38 @@ import isAbsouteUrl from 'is-absolute-url';
 
 emmet(CodeMirror);
 const defaultValue = `<template>
-  <div>Hello {{ msg }}</div>
+  <div>
+    <h2>Hello {{ msg }}</h2>
+    <h3>Demo</h3>
+    <ul>
+      <li v-for="url in urls">
+        <a target="_blank" :href="url">{{ url }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    msg: 'Vuep.run'
-  }) 
+    msg: 'Vuep.run',
+
+    urls: [
+      'https://vuep.run/QingWei-Li/vue-trend/docs/home.vue',
+      'https://vuep.run/QingWei-Li/vuep.run/examples/element-ui.vue?pkg=element-ui&css=element-ui/lib/theme-chalk/index.css'
+    ]
+  })
 }
 <\/script>
 
 <style>
 div {
-  font-size: 20px;
+  color: #2c3e50;
+  font-family: Arial, sans-serif;
+}
+
+a {
+  color: #42b983;
 }
 </style>`;
 
